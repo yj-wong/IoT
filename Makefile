a3b5b0bea03fa99aa22e662a159a77a9f798f6f6
YJ Wong <yujuewong15@gmail.com>
2018-10-12 21:57:42 +0000

    updated alarm and Makefile

--- Makefile
+++ Makefile
@@ -1,12 +1,15 @@
 CC=gcc
 CFLAGS=-lWarn -pedantic
-LDFLAGS=lwiringPi
+LDFLAGS=-lmyifttt -lcurl -lwiringPi
 
 tester: tester.o libmyifttt.a
 	$(CC) tester.o -L. -lmyifttt -lcurl -o tester	
 
-irtester: irtester.c 
-	$(CC) $(CSFLAGS) $(LDFLAGS)
+alarm: alarm.o libmyifttt.a
+	$(CC) alarm.o -L. $(LDFLAGS) -o alarm
+
+alarm.o: alarm.c ifttt.h
+	$(CC) $(CSFLAGS) $(LDFLAGS) -c -ansi $<
 
 libmyifttt.a:	ifttt.o
 	ar -rcs libmyifttt.a ifttt.o
@@ -17,6 +20,10 @@ ifttt.o: 	ifttt.c ifttt.h
 tester.o:	tester.c ifttt.h
 	$(CC) $(CFLAGS) -c -ansi $<
 
+irtester: irtester.c
+	 $(CC) $(CSFLAGS) -lwiringPi -o irtester -ansi $<
+
 clean:
 	rm tester *.o
 
+all: tester alarm irtester
--- alarm.c
+++ alarm.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <wiringPi.h>
+#include "ifttt.h"
+
 int main(int argc, char *argv[])
 {
   wiringPiSetup () ;
@@ -8,16 +10,18 @@ int main(int argc, char *argv[])
   pinMode(2, OUTPUT);
 
   while(1) {
-    printf("Waiting for reset\n");
-    if(digitalRead(0) == 1) {
-      printf("Waiting for event\n");
-      digitalWrite(2, 0);
-      digitalWrite(1, 1);
+    /*printf("Waiting for reset\n");*/
+    while(digitalRead(0) == 0) {
+      /*printf("Waiting for event\n");*/
+      digitalWrite(2, LOW);
+      digitalWrite(1, HIGH);
     }
-    else if(digitalRead(0) == 0) {
+   if(digitalRead(0) == 1) {
       printf("Alarm\n");
-      digitalWrite(1, 0);
-      digitalWrite(2, 1);
+      digitalWrite(1, LOW);
+      digitalWrite(2, HIGH);
+      ifttt("http://red.eecs.yorku.ca:8080/trigger/event/with/key/123", "my1", "my 2", "my 33333");
+      delay(0);
     }
   }
   /*NOTREACHED*/
